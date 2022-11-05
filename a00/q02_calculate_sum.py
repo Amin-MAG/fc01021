6f5ce26a3e1bf6f0e812f7cbd510204268d2942f
Amin-MAG <M.Amin.Ghasvari@Gmail.com>
2022-11-06 00:23:47 +0330

    Add a00

--- a00/q02_calculate_sum.py
+++ a00/q02_calculate_sum.py
@@ -3,4 +3,4 @@ from typing import Tuple
 def calculate_sum(numbers: Tuple[int]) -> int:
     # You need to modify this function.
     # Return the correct answer
-    return 0
+    return int(sum(numbers))
--- a00/q03_count_strings.py
+++ a00/q03_count_strings.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 
-def count_strings(name: Tuple) -> int:
+def count_strings(names: Tuple) -> int:
     # You need to modify this function.
     # Return the correct answer
-    return 0
+    return len(list(filter(lambda item: isinstance(item, str), names)))
+
